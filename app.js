627ebc00522cfc9eefa2271aef040c4246cc855a
paradox <101551548+Sayrayt@users.noreply.github.com>
2024-06-07 03:03:04 +0400

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const UserManager = require('./application/modules/UserManager/UserManager');
 
 const userManager = new UserManager;
 //
+//
 
 const Router = require('./application/routers/Router');
 app.use(express.static('public'));
